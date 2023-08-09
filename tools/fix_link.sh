359964d33cc321185535d7164b9ba0923a7e2599
Richard Albert Leddy <858872+rleddy@users.noreply.github.com>
2023-08-08 22:34:01 -0700

    force npm to make a link

--- tools/fix_link.sh
+++ tools/fix_link.sh
@@ -1,5 +1,5 @@
 
 
 pushd $(npm config get prefix)/bin
-# sudo ln -s ../lib/node_modules/serve-wasm-http/bin/wasm_serv.js serve-wasm-http
+sudo ln -s ../lib/node_modules/serve-wasm-http/bin/wasm_serv.js serve-wasm-http
 popd
